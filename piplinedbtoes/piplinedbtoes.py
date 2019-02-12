eb04a3b340f619872b844ee9b8ad12b30557e517
Geoff Irwin <girwin@Marine.ie>
2019-02-12 14:37:17 +0000

    woking pipleine - cfr only

--- piplinedbtoes/piplinedbtoes.py
+++ piplinedbtoes/piplinedbtoes.py
@@ -2,11 +2,14 @@ import pyodbc
 import json, os
 import requests
 import collections
+import time
+
+start = time.time()
 
 server = 'vminformdev01' 
 database = 'GI_VS_SC_Test' 
 es_base_url = 'http://10.11.1.70:9200/'
-es_index_name = 'foo'
+es_index_name = 'mostrecentcfr'
 headers = {'Content-Type':'application/json'}
 
 #delete and create index in ES
@@ -17,17 +20,18 @@ requests.put(es_base_url + es_index_name)
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes')
 cursor = cnxn.cursor()
 
-cursor.execute("SELECT * FROM FOO;") 
+cursor.execute("SELECT * FROM [InformaticsLoad].[dbo].[Most Recent CFR activity];") 
 
 objects_list = []
 for row in cursor:
     d = collections.OrderedDict()
-    d['fookey'] = row.fooKey
-    d['foovalue'] = row.foofooValue
-    d['anotherValue'] = row.anotherValue
+    d['cfr'] = row.myCFR
     objects_list.append(d)
     j = json.dumps(objects_list[0])
     r = requests.post(es_base_url + es_index_name + '/_doc?pretty',headers = headers, data = j)  
     objects_list.clear()
 
 cnxn.close()
+
+end = time.time()
+print(end - start)
