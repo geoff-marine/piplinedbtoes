27e52c5e6a1f7fd8b8a54b5fcaba409069be2bcc
Geoff Irwin <geoff.irwin@Marine.ie>
2019-03-12 11:21:12 +0000

    added init for date type

--- piplinedbtoes/piplinedbtoes.py
+++ piplinedbtoes/piplinedbtoes.py
@@ -21,6 +21,49 @@ headers = {'Content-Type':'application/json'}
 es_input_open = '{"input":['
 es_input_close = ']}' 
 
+es_init_for_all_events = '''
+{
+    "mappings":{
+        "allevents": {
+            "properties": {
+                "VesselName" : {
+                    "type": "keyword"
+                },
+                "cfr" : {
+                    "type": "keyword"
+                },
+                "CountryCode" : {
+                    "type": "keyword"
+                },
+                "PortCode" : {
+                    "type": "keyword"
+                },
+                "PortName" : {
+                    "type": "keyword"
+                },
+                "Loa" : {
+                    "type": "keyword"
+				},
+                "Lbp" : {
+                    "type": "keyword"
+                },
+                "EventCode" :{
+                    "type" : "keyword"
+                },
+                "EventStartDate" : {
+                    "type" : "date",
+                    "format": "yyyy-MM-dd HH:mm:ss"
+                },
+                "EventEndDate": {
+                    "type": "date",
+                    "format": "yyyy-MM-dd HH:mm:ss"
+                }
+                
+                }
+            }
+        }
+}
+'''
 
 es_init_for_all_most_recent_cfrs = '''
 {
@@ -175,6 +218,7 @@ es_init_for_all_vesslNames = '''
 #no es init needed as each field will be a keyword
 
 requests.delete(es_base_url + es_index_name_all)
+requests.put(es_base_url + es_index_name_all, headers = headers, data = es_init_for_all_events)
 
 cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes')
 cursor = cnxn.cursor()
@@ -185,7 +229,7 @@ objects_list = []
 collection_objects =[]
 
 while True:
-    results = cursor.fetchmany(200000)
+    results = cursor.fetchmany(20000)
     if not results:
         break
     for row in results:
@@ -216,47 +260,48 @@ cnxn.close()
 
 #trasnfer data from sql server to es for all records in most recent cfrs view
 
-vessel_cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes')
+#vessel_cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes')
 
-requests.put(es_base_url + es_index_name_one_cfr, headers = headers, data = es_init_for_all_most_recent_cfrs)
+#requests.put(es_base_url + es_index_name_one_cfr, headers = headers, data = es_init_for_all_most_recent_cfrs)
 
-cursor = vessel_cnxn.cursor()
-cursor.execute("SELECT [CFR], [Country Code], [Vessel Name], [Port Code], [Port Name], [Loa], [Lbp] FROM [InformaticsLoad].[dbo].[Most Recent CFR activity]")
+#cursor = vessel_cnxn.cursor()
+#cursor.execute("SELECT [CFR], [Country Code], [Vessel Name], [Port Code], [Port Name], [Loa], [Lbp] FROM [InformaticsLoad].[dbo].[Most Recent CFR activity]")
 
-objects_list = []
-collection_objects =[]
+#objects_list = []
+#collection_objects =[]
 
-while True:
-    results = cursor.fetchmany(20000)
-    if not results:
-        break
-    for row in results:
-        d = collections.OrderedDict()
-        d['cfr'] = row.CFR
-        d['CountryCode'] = row.__getattribute__('Country Code')
-        d['VesselName'] = row.__getattribute__('Vessel Name')
-        d['ExactName'] = row.__getattribute__('Vessel Name')
-        d['PortCode'] = row.__getattribute__('Port Code')
-        d['PortName'] = row.__getattribute__('Port Name')
-        d['Loa'] = row.Loa
-        d['Lbp'] = row.Lbp
-        objects_list.append(d)
-        j = json.dumps(objects_list[0], default = str)
-        t = '{"index":{"_index":"' + es_index_name_one_cfr[:-1] +'", "_type":"' + es_type_name_cfr + '", "_id":"' + row.CFR + '"} }\n'
-        n = '\n'
-        p = t + j + n
-        collection_objects.append(p)
-        objects_list.clear()
-    send_to_es = ''.join(collection_objects) + n
-    r = requests.put(es_base_url + es_index_name_one_cfr + '_bulk',headers = headers, data = send_to_es) 
-    collection_objects.clear()
+#while True:
+#    results = cursor.fetchmany(20000)
+#    if not results:
+#        break
+#    for row in results:
+#        d = collections.OrderedDict()
+#        d['cfr'] = row.CFR
+#        d['CountryCode'] = row.__getattribute__('Country Code')
+#        d['VesselName'] = row.__getattribute__('Vessel Name')
+#        d['ExactName'] = row.__getattribute__('Vessel Name')
+#        d['PortCode'] = row.__getattribute__('Port Code')
+#        d['PortName'] = row.__getattribute__('Port Name')
+#        d['Loa'] = row.Loa
+#        d['Lbp'] = row.Lbp
+#        objects_list.append(d)
+#        j = json.dumps(objects_list[0], default = str)
+#        t = '{"index":{"_index":"' + es_index_name_one_cfr[:-1] +'", "_type":"' + es_type_name_cfr + '", "_id":"' + row.CFR + '"} }\n'
+#        n = '\n'
+#        p = t + j + n
+#        collection_objects.append(p)
+#        objects_list.clear()
+#    send_to_es = ''.join(collection_objects) + n
+#    r = requests.put(es_base_url + es_index_name_one_cfr + '_bulk',headers = headers, data = send_to_es) 
+#    collection_objects.clear()
 
-vessel_cnxn.close()
+#vessel_cnxn.close()
 
 #vessel names
 
 vesselname_cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=yes')
 
+requests.delete(es_base_url + es_index_name_by_vesselName)
 requests.put(es_base_url + es_index_name_by_vesselName, headers = headers, data = es_init_for_all_vesslNames)
 
 cursor = vesselname_cnxn.cursor()
